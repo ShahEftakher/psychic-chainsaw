f23410d87ecf529b1ddcc7f29dc0451e2bd66a52
ShahEftakher <shaheftakher@iut-dhaka.edu>
2022-09-30 16:14:49 +0600

    1 complete

--- TwoSum_1.java
+++ TwoSum_1.java
@@ -2,14 +2,22 @@ import java.util.HashMap;
 
 public class TwoSum_1 {
     public static void main(String[] args) {
-
+        int[] arr = new int[] { 1 };
+        int[] result = twoSum(arr, 2);
+        for (int i = 0; i < result.length; i++) {
+            System.out.println(result[i]);
+        }
     }
 
-    public int[] twoSum(int[] nums, int target) {
+    public static int[] twoSum(int[] nums, int target) {
         HashMap<Integer, Integer> hashMap = new HashMap<Integer, Integer>();
-        for(int i=0;i<nums.length;i++){
-            
+        for (int i = 0; i < nums.length; i++) {
+            int _target = target - nums[i];
+            if (hashMap.containsKey(_target)) {
+                return new int[] { i, hashMap.get(_target) };
+            }
+            hashMap.put(nums[i], i);
         }
-        return new int[] { 3, 4, 4 };
+        return nums;
     }
 }
