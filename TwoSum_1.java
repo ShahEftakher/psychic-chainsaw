5296ecc16b577f6ca7198ebe3cb97aba2c9003dd
ShahEftakher <shaheftakher@iut-dhaka.edu>
2022-09-30 14:11:34 +0600

    Started 1

---
+++ TwoSum_1.java
@@ -0,0 +1,15 @@
+import java.util.HashMap;
+
+public class TwoSum_1 {
+    public static void main(String[] args) {
+
+    }
+
+    public int[] twoSum(int[] nums, int target) {
+        HashMap<Integer, Integer> hashMap = new HashMap<Integer, Integer>();
+        for(int i=0;i<nums.length;i++){
+            
+        }
+        return new int[] { 3, 4, 4 };
+    }
+}
