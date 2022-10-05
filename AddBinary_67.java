c7de5feeaecdd510eb89ed33101682a57237d517
ShahEftakher <shaheftakher@iut-dhaka.edu>
2022-10-06 01:17:33 +0600

    Started 2 more

---
+++ AddBinary_67.java
@@ -0,0 +1,9 @@
+public class AddBinary_67 {
+    public static void main(String[] args) {
+
+    }
+
+    public String addBinary(String a, String b) {
+        return "meh";
+    }
+}
---
+++ PalindromeNumber_9.java
@@ -0,0 +1,11 @@
+public class PalindromeNumber_9 {
+    public static void main(String[] args) {
+        System.out.println(1233 / 10);
+        System.out.println(1233 % 10);
+    }
+
+    public static boolean isPalindrome(int x) {
+
+        return true;
+    }
+}
